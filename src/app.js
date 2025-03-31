294cde5244de09bf0f6294eafce9949505c72da9
Gercius <gerciuswork@gmail.com>
2025-03-31 09:08:25 +0300

    Add cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const autoRepairShopRouter = require("./routes/autoRepairShopRoutes");
 const autoMechanicRouter = require("./routes/autoMechanicRoutes");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/auto-repair-shop", autoRepairShopRouter);
 app.use("/api/auto-mechanic", autoMechanicRouter);
